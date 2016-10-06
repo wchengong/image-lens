10c42f81fb7f0a67eff91149700006221d9d3e12
wchengong <wchengong@vcd-ch204-03.eastern.ewu.edu>
2016-10-06 14:17:20 -0700

    version 1
    heheda

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+
+//text scripts.js
+console.log('ok usa!')
+
+//text jquary
+//$('img').hide();
+
+
+$('img').imageLens({
+    imageSrc: '/img/watch-zoom.jpg',
+    lensSize: 400,
+    borderColor: 'yellow'
+    });
